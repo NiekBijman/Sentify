08c411530d8c9fcc05e4db515c8133b50db7852b
bijman <bijman@kth.se>
2018-04-06 23:30:50 +0200

    Pie Chart / Input Box (In Progress)
    - Pie.js contains Container/Presentation components
    - API call has to be added
    - Loading sign added

---
+++ src/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import loading from './loading.gif'
+
+const Loading = () => (
+  <div>
+    <img src={loading} alt='loading'/>
+  </div>
+);
+
+export default Loading;
--- src/data/Model.js
+++ src/data/Model.js
@@ -18,16 +18,6 @@ const Model = function () {
   this.getContainer = function(){
     return container;
   }
-
-  // this.getMap = function() {
-  //     var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-  //     mapboxgl.accessToken = 'pk.eyJ1Ijoibmlla2Jpam1hbiIsImEiOiJjamY0MnN2NXkxaGpjMzRwZHloM3FoZG9uIn0.eZBRbD2LO-4yNS-gXVtRag';
-  //     var map = new mapboxgl.Map({
-  //       container: this.getContainer(),
-  //       style: 'mapbox://styles/mapbox/streets-v10'
-  //     });
-  // }
-
   // Observer pattern
 
   this.addObserver = function (observer) {
