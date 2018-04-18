db3dd6c2b52b684afc36b95ad7161b7c84cf4b45
bijman <bijman@kth.se>
2018-04-18 13:08:50 +0200

    test with priority serving static files

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 5000;
 // });
 
 // Priority serve any static files.
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
