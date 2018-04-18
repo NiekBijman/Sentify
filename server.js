fcb4d3460444af8380cc5bc9eacd0a40e9e749da
bijman <bijman@kth.se>
2018-04-18 12:48:00 +0200

    cd to folder where index.html is location

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var TW_KEYS = {
 const app = express();
 const fetchTweets = new FetchTweets(TW_KEYS);
 
-const port = process.env.SENTIFY_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // Express only serves static assets in production
 // if (process.env.NODE_ENV === 'production') {
@@ -29,6 +29,14 @@ const port = process.env.SENTIFY_PORT || 5000;
 //     // ejs render automatically looks in the views folder
 //     res.render('index');
 // });
+
+// Priority serve any static files.
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+});
 
 // To silence favico.ico errors. Ignore.
 app.get('/client/src/media/favicon.ico', (req, res) => {
